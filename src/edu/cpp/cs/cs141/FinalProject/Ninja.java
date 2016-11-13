d0cd4b1bdb811351788007c60143c45c5cf7d46f
victorthis <victoryuen@live.com>
2016-11-13 10:19:02 -0800

    Update Ninja.java

--- src/edu/cpp/cs/cs141/FinalProject/Ninja.java
+++ src/edu/cpp/cs/cs141/FinalProject/Ninja.java
@@ -11,5 +11,16 @@ package edu.cpp.cs.cs141.FinalProject;
  *
  */
 public class Ninja extends ActiveAgent {
-
+  
+  /**
+	 * This represents the status of the Ninja.
+	 */
+	private boolean alive;
+	
+	/**
+	 * This represents the number of lives the ninja has left which is by default 1.
+	 */
+	private int lives = 1;
+	
+  
 }
