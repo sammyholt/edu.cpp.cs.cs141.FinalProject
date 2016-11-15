7274f531d226c2dc713c336b24b5fcb58faf85ef
khatastrophes <justinkhado@gmail.com>
2016-11-15 02:57:07 -0800

    Add files via upload

--- Player.java
+++ Player.java
@@ -28,6 +28,69 @@ public class Player extends ActiveAgent {
 	 */
 	private int ammo;
 	
-	private Item item;
+	/**
+	 * This represents whether or not the Player has the briefcase.
+	 */
+	private boolean briefcase;
+	
+	
+	
+		
+	/**
+	 * The constructor for the Player class which initializes the field variables.
+	 */
+	public Player()
+	{
+		alive = true;
+		lives = 3;
+		briefcase = false;
+		ammo = 1;
+				
+	}
+	
+	/**
+	 * @return the number of lives the Player has remaining.
+	 */
+	public int getLives()
+	{
+		return lives;
+	}
+	
+	/**
+	 * If the player is killed, the life total is deducted.
+	 */
+	public void changeLife()
+	{
+		if (lives >= 0)
+			lives--;
+		if(lives == 0)
+			alive = false;
+	}
+	
+	/**
+	 * @return alive is true if the Player still has lives remaining, otherwise returns false
+	 */
+	public boolean isAlive()
+	{
+		return alive;
+	}
+	
+	/**
+	 * (@code briefcase) is set to true after the Player has gotten the briefcase.
+	 */
+	public void setBriefcase()
+	{
+		briefcase = true;
+	}
+	
+	/**
+	 * @return true if the Player has gotten the briefcase, otherwise returns false
+	 */
+	public boolean hasBriefcase()
+	{
+		return briefcase;
+	}
+	
+	
 
 }
